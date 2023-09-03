0c81199290f205a98eb7fd218f648b2dc2fa1907
David Weber <weberdavid91@gmail.com>
2023-09-03 11:09:00 +0200

    Restructured code

---
+++ src/main/java/com/wedasoft/javafxprojectgenerator/MainApplicationLauncher.java
@@ -0,0 +1,9 @@
+package com.wedasoft.javafxprojectgenerator;
+
+public class MainApplicationLauncher {
+
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.wedasoft.javafxprojectgenerator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.wedasoft.javafxprojectgenerator to javafx.fxml;
-    exports com.wedasoft.javafxprojectgenerator;
-}
