47c63d0ebac6ddc01998c5d5f16346da46bfe085
David Weber <weberdavid91@gmail.com>
2023-12-03 11:38:07 +0100

    added exit-view

--- src/main/java/com/wedasoft/javafxprojectgenerator/views/main/MainViewController.java
+++ src/main/java/com/wedasoft/javafxprojectgenerator/views/main/MainViewController.java
@@ -1,11 +1,15 @@
 package com.wedasoft.javafxprojectgenerator.views.main;
 
+import com.wedasoft.javafxprojectgenerator.helper.DialogHelper;
 import com.wedasoft.javafxprojectgenerator.helper.HelperFunctions;
 import com.wedasoft.javafxprojectgenerator.views.createProject.CreateProjectViewController;
+import com.wedasoft.javafxprojectgenerator.views.exit.ExitViewController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainViewController {
 
@@ -38,10 +42,18 @@ public class MainViewController {
     }
 
     public void onAboutButtonClick() throws IOException {
-        //        HelperFunctions.switchBorderPaneCenter(
-        //                borderPane,
-        //                getClass().getResource("/com/wedasoft/javafxprojectgenerator/views/createProject/create-project.fxml"),
-        //                controller -> ((CreateProjectViewController) controller).init());
+        DialogHelper.displayDialogWithColumns("About", 2, List.of(
+                new Label("Developed by:"),
+                new Label("David Weber"),
+                new Label("Homepage:"),
+                new Label("www.wedasoft.com")));
+    }
+
+    public void onExitButtonClick() throws IOException {
+        HelperFunctions.switchBorderPaneCenter(
+                borderPane,
+                getClass().getResource("/com/wedasoft/javafxprojectgenerator/views/exit/exit-view.fxml"),
+                controller -> ((ExitViewController) controller).init());
     }
 
 }
